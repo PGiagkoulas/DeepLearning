576d0a995d4b428bcf394a92558106e89b7bf456
lukas <j.l.edman@student.rug.nl>
2019-05-01 02:42:40 +0200

    pretrained flag, resnet + densenet, model_name test will not save

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from keras.datasets import cifar10, cifar100
 from networks import *
 from utils import load_model, save_model_architecture
 
-bool_flag = lambda x: True if x.lower() in {'true', '1'} else False
-
 parser = argparse.ArgumentParser(description='Run a model.')
-parser.add_argument('--model_name', type=str, default='test2',
+parser.add_argument('--model_name', type=str, default='test',
 					help='Name of the model. Loads model with same name automatically.')
-parser.add_argument('--architecture', type=str, default='all_conv',
+parser.add_argument('--architecture', type=str, default='densenet',
 					help='Architecture to use. Note: this will be ignored if model_name is a different architecture.')
+parser.add_argument('--pretrained', action='store_true',
+					help='Use "--pretrained" for a model pretrained on imagenet. VGG/ResNet/Inception only.')
 parser.add_argument('--dataset', type=str, default='cifar100',
 					help='Dataset to use. [cifar10/cifar100]')
 parser.add_argument('--save_interval', type=int, default=1,
@@ -28,6 +28,8 @@ parser.add_argument('--n_epochs', type=int, default=1,
 args = parser.parse_args()
 args.model_path = os.path.join('models', args.model_name)
 args.initial_epoch = 0 
+
+args.pretrained =True
 
 if not os.path.isdir('models'):
 	os.mkdir('models')
@@ -44,7 +46,9 @@ y_test = to_categorical(y_test)
 
 
 # --- LOAD MODEL ---
-if os.path.isdir(args.model_path):
+if args.model_name == 'test':
+	model = MODELS[args.architecture](args)	
+elif os.path.isdir(args.model_path):
 	model = load_model(args)
 else:
 	os.mkdir(args.model_path)
@@ -52,22 +56,35 @@ else:
 	save_model_architecture(model, args)
 
 if __name__ == '__main__':
+	
 	checkpt = ModelCheckpoint(
 		os.path.join(args.model_path,'weights.ep{epoch:03d}.val{val_acc:.3f}.hdf5'), 
 		save_weights_only=True, 
 		period=args.save_interval)
+
 	model.compile(
 				#optimizer='adam',
 		optimizer=rmsprop(lr=0.0001, decay=1e-6),
 		loss='categorical_crossentropy', 
 		metrics=['accuracy'])
-	model.fit(
-		x_train, 
-		y_train, 
-		validation_data=(x_test, y_test),
-		initial_epoch=args.initial_epoch, 
-		epochs=args.n_epochs+args.initial_epoch, 
-		batch_size=args.batch_size, 
-		callbacks=[checkpt])
+	
+
+	if args.model_name == 'test':
+		model.fit(
+			x_train, 
+			y_train, 
+			validation_data=(x_test, y_test),
+			initial_epoch=args.initial_epoch, 
+			epochs=args.n_epochs+args.initial_epoch, 
+			batch_size=args.batch_size)
+	else:
+		model.fit(
+			x_train, 
+			y_train, 
+			validation_data=(x_test, y_test),
+			initial_epoch=args.initial_epoch, 
+			epochs=args.n_epochs+args.initial_epoch, 
+			batch_size=args.batch_size, 
+			callbacks=[checkpt])
 	# model.evaluate(x_test, y_test,
 	# 	batch_size=args.batch_size)
--- networks.py
+++ networks.py
@@ -1,7 +1,11 @@
 # networks.py
-from keras.layers import Dense, Activation,  Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D
-from keras.models import Sequential
+from functools import partial
 
+from keras.layers import Dense, Activation,  Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D, Dropout
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+from keras.applications.densenet import DenseNet121
 
 def all_conv_net(args):
 	model = Sequential()
@@ -24,44 +28,61 @@ def all_conv_net(args):
 
 
 def simple_conv_net(args):
-		model = Sequential()
-		model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
-		model.add(Activation('relu'))
-		model.add(Conv2D(32, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+	model.add(Activation('relu'))
+	model.add(Conv2D(32, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
-		model.add(Conv2D(64, (3, 3), padding='same'))
-		model.add(Activation('relu'))
-		model.add(Conv2D(64, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+	model.add(Conv2D(64, (3, 3), padding='same'))
+	model.add(Activation('relu'))
+	model.add(Conv2D(64, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
-		model.add(Flatten())
-		model.add(Dense(512))
-		model.add(Activation('relu'))
-		model.add(Dropout(0.5))
-		model.add(Dense(args.n_outputs))
-		model.add(Activation('softmax'))
-		return model
+	model.add(Flatten())
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(args.n_outputs))
+	model.add(Activation('softmax'))
+	return model
 
-def pre_vgg16_net(args):
-		model = VGG16(weights="imagenet", include_top=False)
-		# freeze conv layers
-		for layer in model.layers:
-			layer.trainable = False
-		model.add(Flatten())
-		model.add(Dense(512))
-		model.add(Activation('relu'))
-		#model.add(Dropout(0.5))
-		model.add(Dense(args.n_outputs))
-		model.add(Activation('softmax'))
-		return model
-		
+def prebuilt_model(args, model):
+    """ Any of the pre-built keras models """
+    if args.pretrained:
+        resnet = model(weights='imagenet', input_shape=(32,32,3), include_top=False)
+        for layer in resnet.layers:
+            layer.trainable = False
+        model = Sequential()
+        model.add(resnet)
+        model.add(Flatten())
+        model.add(Dense(args.n_outputs))
+        model.add(Activation('softmax'))
+    else:
+        model = model(weights=None, input_shape=(32,32,3), include_top=True, classes=args.n_outputs)
+    return model    
+
+# def pre_vgg16_net(args):
+# 	model = VGG16(weights="imagenet", include_top=False)
+# 	# freeze conv layers
+# 	for layer in model.layers:
+# 		layer.trainable = False
+# 	model.add(Flatten())
+# 	model.add(Dense(512))
+# 	model.add(Activation('relu'))
+# 	#model.add(Dropout(0.5))
+# 	model.add(Dense(args.n_outputs))
+# 	model.add(Activation('softmax'))
+# 	return model
+	
 MODELS = {
 	'all_conv': all_conv_net,
-		'simple_conv': simple_conv_net,
-		'pre_vgg16': pre_vgg16_net
+	'simple_conv': simple_conv_net,
+	'vgg16': partial(prebuilt_model, model=VGG16),
+    'resnet50': partial(prebuilt_model, model=ResNet50),
+    'densenet': partial(prebuilt_model, model=DenseNet121)
 }
