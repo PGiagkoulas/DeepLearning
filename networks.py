176a1f4da1af6ecdb1d53ad7da39c075acbc4307
Panagiotis <pgiagkoulas@gmail.com>
2019-05-04 11:04:33 +0200

    Lenet5 and previous vgg removal

--- networks.py
+++ networks.py
@@ -1,7 +1,7 @@
 # networks.py
 from functools import partial
 
-from keras.layers import Dense, Activation,  Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D, Dropout
+from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D, Dropout
 from keras.models import Sequential
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet50 import ResNet50
@@ -58,96 +58,46 @@ def prebuilt_model(args, model):
 		prebuilt = model(weights='imagenet', input_shape=(32,32,3), include_top=False)
 		for layer in prebuilt.layers:
 			layer.trainable = False
-	else:
-		prebuilt = model(weights=None, input_shape=(32,32,3), include_top=False)
-	model = Sequential()
-	model.add(prebuilt)
-	model.add(Activation('relu'))
-	model.add(Dense(args.n_outputs))
-	model.add(Activation('softmax'))
-
-	return model    
-
-
-
-def vgg16(args):
-	""" Any of the pre-built keras models """
-	if args.pretrained:
-		prebuilt = VGG16(weights='imagenet', input_shape=(32,32,3), include_top=False)
-		for layer in prebuilt.layers:
-			layer.trainable = False
-	else:
-		prebuilt = VGG16(weights=None, input_shape=(32,32,3), include_top=False)
-	model = Sequential()
-	model.add(prebuilt)
-	model.add(Flatten())
-	model.add(Dense(4096))
-	model.add(Activation('relu'))
-	model.add(Dense(4096))
-	model.add(Activation('relu'))
-	model.add(Dense(args.n_outputs))
-	model.add(Activation('softmax'))
-
-	return model    
-
-def alex_net(args):
 		model = Sequential()
-		# 1st Conv + ReLU + MaxPool
-		model.add(Conv2D(93, (11,11), strides=(4,4), padding='valid', input_shape=(32, 32, 3), name='conv1'))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
-		# 2nd Conv + ReLU + MaxPool
-		model.add(Conv2D(256, (5,5), strides=(1,1), padding='valid', name='conv2'))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
-		# 3rd Conv + ReLU
-		model.add(Conv2D(384, (3,3), strides=(1,1), padding='valid', name='conv3'))
-		model.add(Activation('relu'))
-		# 4th Conv + ReLU
-		model.add(Conv2D(384, (3,3), strides=(1,1), padding='valid', name='conv4'))
-		model.add(Activation('relu'))
-		# 5th Conv + ReLU + MaxPool
-		model.add(Conv2D(256, (3,3), strides=(1,1), padding='valid', name='conv5'))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
-		# Flatten
+		model.add(prebuilt)
 		model.add(Flatten())
-		# 1st Fully Connected + ReLU
-		model.add(Dense(9216), name='fc6')
+		model.add(Dense(512))
 		model.add(Activation('relu'))
-		# 2nd Fully Connected + ReLU
-		model.add(Dense(4096), name='fc7')
-		model.add(Activation('relu'))
-		# 3rd Fully Connected + ReLU
-		model.add(Dense(4096), name='fc8')
-		model.add(Activation('relu'))
-		# Output
 		model.add(Dense(args.n_outputs))
 		model.add(Activation('softmax'))
+	else:
+		model = model(weights=None, input_shape=(32,32,3), include_top=True, classes=args.n_outputs)
+	return model    
 
-		
-		return model
+def lenet5(args):
+        model = Sequential()
+        # 1st Conv + ReLU + AvgPool
+        model.add(Conv2D(10, kernel_size=(5,5), input_shape=(32, 32, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        # 2nd Conv + ReLU + AvgPool
+        model.add(Conv2D(32, kernel_size=(5,5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        # Flatten
+        model.add(Flatten())
+        # 3rd FC
+        model.add(Dense(120))
+        model.add(Activation('relu'))
+        # 4th FC
+        model.add(Dense(84))
+        model.add(Activation('relu'))
+        # Output
+        model.add(Dense(10))
+        model.add(Activation('softmax'))
 
+        return model
 
-
-# def pre_vgg16_net(args):
-# 	model = VGG16(weights="imagenet", include_top=False)
-# 	# freeze conv layers
-# 	for layer in model.layers:
-# 		layer.trainable = False
-# 	model.add(Flatten())
-# 	model.add(Dense(512))
-# 	model.add(Activation('relu'))
-# 	#model.add(Dropout(0.5))
-# 	model.add(Dense(args.n_outputs))
-# 	model.add(Activation('softmax'))
-# 	return model
-	
 MODELS = {
 	'all_conv': all_conv_net,
 	'simple_conv': simple_conv_net,
-	'alex_net': alex_net,
-	'vgg16': vgg16,
+	'lenet5': lenet5,
+	'vgg16': partial(prebuilt_model, model=VGG16),
 	'resnet50': partial(prebuilt_model, model=ResNet50),
-	'densenet': partial(prebuilt_model, model=DenseNet121)
+	'densenet': partial(prebuilt_model, model=DenseNet121),
 }
